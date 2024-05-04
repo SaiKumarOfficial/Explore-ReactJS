9df3cbe10446958c5772d6e858b2d6ebd0407113
saikumar <sai680513@gmail.com>
2024-05-04 11:08:06 +0530

    build react app using vite framework

---
+++ 01basicvite/src/App.tsx
@@ -0,0 +1,16 @@
+import Youtube from  "./youtube"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Classic React Vite app</h1>
+    <h1>Classic React Vite app</h1>
+    <Youtube/>
+    </>
+  )
+}
+
+export default App
